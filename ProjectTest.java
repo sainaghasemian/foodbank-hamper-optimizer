c1463d1a252c020aeace8ce8c322ec59d3cb35ad
Ana Perrone <anaclara.perrone@gmail.com>
2022-03-30 17:54:10 -0600

    Update ProjectTest.java
    Added skeleton for hamper tests and one client test

--- ProjectTest.java
+++ ProjectTest.java
@@ -1,12 +1,39 @@
 package edu.ucalgary.ensf409;
 
 
+import java.beans.Transient;
 import java.util.regex.*;
 import org.junit.Test;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 
 public class ProjectTest {
+
+    @Test
+    public void testHamperConstructor() {
+
+    }
+
+    @Test
+    public void testSetFood() {
+        
+    }
+
+    @Test 
+    public void testGetFood() {
+       
+    }
+
+    @Test
+    public void testGetClients() {
+        
+    }
+
+    @Test 
+    public void testGetNutrition() {
+
+    }
+    
     /*
     Constructor Tests
     */
