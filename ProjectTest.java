7dc87911f72c69090cefcbeee780bd107a824892
Saina Ghasemian <76859857+sainaghasemian@users.noreply.github.com>
2022-03-30 17:10:00 -0600

    Update ProjectTest.java
    Test Descriptions

--- ProjectTest.java
+++ ProjectTest.java
@@ -1,3 +1,23 @@
 package edu.ucalgary.ensf409;
 
-public class prkfhd
+
+import java.util.regex.*;
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+
+public class ProjectTest {
+    /*
+    Constructor Tests
+    */
+    /*
+    Method Tests
+    */
+    /*
+    Throw Tests
+    */
+    /*
+    Enumeration Method Tests
+    */
+
+}
